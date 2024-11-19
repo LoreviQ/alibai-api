b93328bb85802a7fc4e6f492eaea58a6748dc9bc
LoreviQ <lorevi@protonmail.com>
2024-11-19 00:35:05 +0000

    Changed back to main.py method of running

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 """Main file setsup and runs the flask app."""
 
+import os
+
 from flask import Flask
 from flask_cors import CORS
 
@@ -8,3 +10,7 @@ from routes import register_routes
 app = Flask(__name__)
 CORS(app)
 register_routes(app)
+port = int(os.getenv("PORT", "5000"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
